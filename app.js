88e1e50ed8a8017311819180b22e3dc1301580d2
Alejandro Arango <alejandroarang@gmail.com>
2023-01-10 16:05:30 -0500

    funcionando conexion desde home

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const express = require('express');
-const app= express();
+const app = express();
 const path = require('path');
 const { clearScreenDown } = require('readline');
-const PORT =3030;
+const PORT = 3030;
 
 
 
 app.use(express.static('public'));
-// app.use(express.static('views'));
+app.use(express.static('views'));
 
 
-app.get('/', (req,res)=>{
-    
-    res.sendFile(path.resolve(__dirname, './views/ProductCart.html'))
-    // res.sendFile(path.resolve(__dirname, '/views/home.html'))
-    // res.sendFile(path.resolve(__dirname, './views/login.html'))
-    // res.sendFile(path.resolve(__dirname, './views/product.html'))
-    // res.sendFile(path.resolve(__dirname, './views/register.html'))
-
-
-
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/ProductCart.html'))
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/product.html'))
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
 
-app.listen(PORT,()=>{
-    console.log(`Server run puerto on ${PORT}` );
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server run puerto on ${PORT}`);
 })
