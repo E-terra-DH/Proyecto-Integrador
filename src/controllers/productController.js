27f934c5faa260aa39382450d10165ecddea6713
Alejandro Arango <alejandroarang@gmail.com>
2023-02-15 20:06:05 -0500

    rutas al 60% json cambiar nombre

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,18 +1,57 @@
-const productController =  {
-    productDetail: (req, res) => {
-        res.render('./products/productDetail');
-   },
-   productCart: (req, res) => {
-        res.render('./products/productCart');
-   },    
-   productList: (req, res) => {
-        res.render('./products/productList');
-   },    
-   formLoad: (req, res) => {
-        res.render('./formLoad');
-   },    
-   formEdit: (req, res) => {
-        res.render('./formularioEdit');
-   }
- };
- module.exports = productController;
+const fs = require('fs');
+const path = require('path');
+const { stringify } = require('querystring');
+const productsPath = path.join(__dirname, '../data/products.json');
+const dataBaseProducts = JSON.parse(fs.readFileSync(productsPath, 'utf-8'));
+
+
+
+
+const productController = {
+     /*getPlantas: () => {
+          return JSON.parse(fs.readFileSync(plantasPath, 'utf-8'));
+     },*/
+     index: (req, res) => {
+
+          res.render('./products/index', {
+               title: 'Listado de productos',
+               plantasList: dataBaseProducts
+          });
+     }
+     ,
+     productDetail: (req, res) => {
+          res.render('./products/productDetail')
+     },
+     productCart: (req, res) => {
+          res.render('./products/productCart');
+     },
+     create: (req, res) => {
+          res.render('./products/formLoad');
+     },
+     store: (req, res) => {
+          // TODO:crear la logica
+     },
+     edit: (req, res) => {
+
+          res.render('./products/formularioEdit');
+     },
+     update: (req, res) => {
+
+
+          //TODO:Implementar logica
+     },
+
+
+     productList: (req, res) => {
+          res.render('./products/productList');
+     },
+     formLoad: (req, res) => {
+          res.render('./formLoad');
+     },
+     formEdit: (req, res) => {
+          res.render('./formularioEdit');
+     }
+};
+
+
+module.exports = productController;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,11 +4,46 @@ const path = require('path');
 
 const productController = require('../controllers/productController');
 
+
+
+// Ruta a lista de products
 router.get('/',productController.productList);
-router.get('/detail',productController.productDetail);
+
+//Creando indice de productos disponibles
+router.get('/index',productController.index);
+
+// CREAR Productos usando el formulario
+router.get('/create',productController.create);
+router.post('/create',productController.store);
+
+//Actualizar o crear productos
+router.get('/edit', productController.edit );
+router.put('/edit/:id', productController.update );// se usa para diferenciar cuando se actualiza
+
+
 router.get('/cart',productController.productCart);
+router.get('/detail',productController.productDetail);
 //router.get('/list',productController.productList);
-router.get('/formLoad',productController.formLoad);
-router.get('/formEdit',productController.formEdit);
+
+
+// Rutas a crear
+
+
+
+
+
+
+
+/*
+// ver productos 
+router.get('/:id', productsController.show);
+
+//Eliminar productos
+router.get('/delete/:id', productsController.delete);
+router.delete('/delete/:id', productsController.destroy);//se usa para diferenciar cuando se elimina
+*/
+
+
+
 
 module.exports = router;
