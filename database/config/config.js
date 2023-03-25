a34303ffbd5cfeeac53c855b57dbddde84b85e0c
Alejandro Arango <alejandroarang@gmail.com>
2023-03-25 11:16:06 -0500

    con la bdd dentro

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports ={
   "development": {
     "username": "root",
-    "password": null,
+    "password": "123456",
     "database": "terra",
     "host": "127.0.0.1",
     "dialect": "mysql",
@@ -9,14 +9,14 @@ module.exports ={
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "123456",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": "123456",
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
