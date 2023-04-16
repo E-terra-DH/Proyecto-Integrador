b9dde4f08d7e3be254063fd7f3f4f188c28c6955
valenconde15 <valeconde-2004@hotmail.com>
2023-04-16 11:28:01 -0300

    script de agregar productos actualizado

--- BaseProyecto/BaseTerra.sql
+++ BaseProyecto/BaseTerra.sql
@@ -1,33 +1,4 @@
 --
--- Volcado de datos para la tabla `products`
---
-
-INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `stock`, `products_categories_id`) VALUES
-(1, 'Ave del Paraíso', '50', 'Esta planta originaria de sudáfrica, se cultiva extensamente como planta ornamental por la forma peculiar de su flor.  Es una belleza natural de la que es imposible no enamorarse. Si bien en una maceta puede alcanzar una altura máxima de metro y medio, plantada directamente en suelo puede superar los dos metros y medio. Como buena planta tropical, necesita al menos cuatro horas de sol diarias especialmente en los meses de frío. ', 'product-ave-paraiso.png', 10, 1),
-(2, 'Ficus elastica', '45', 'Es una de esas plantas de interior irresistible. Por un lado, su belleza ornamental es capaz de decorar con la elegancia de sus hojas intensamente verdes. Por otro, los cuidados del ficus elástica son tan sencillos que hacen de esta planta una poco trabajosa. Busca para ella un lugar de muchísima luz, mejor si es al sol directo. Además el riego debe ser moderado', 'ficus-elastica.png', 10, 1),
-(3, 'Syngonium', '55', 'Esta no solo es sumamente hermosa y llamativa. Además es fácil de cuidar así que será sencillo para ti disfrutar de una belleza verde que decora con su mera presencia gracias, en gran medida, a sus hojas. Para cultivarla busca un espacio con buena luminosidad, no permitas que le de el sol directo y dale una buena dosis de humedad en la tierra, pero no demasiada.', 'product-syngonium.png', 10, 1),
-(4, 'Begonia Maculata', '70', 'Esta planta es un auténtico espectáculo natural. Sus singulares lunares plateados resaltan sobre el intenso verde de sus hojas y  sus hojas caen en cascada a partir de su tronco central. Además, sus flores en forma de campana nacen en forma de racimos ininterrumpidamente desde primavera hasta final del verano. Debes cultivarla dentro de tu casa en un lugar de mucha luminosidad pero nunca con sol directo.', 'product-begonia-maculata.png', 10, 1),
-(5, 'Miami', '30', 'Esta es una planta muy versátil, por lo que se puede tener en el interior o exterior. Cuando el Miami se quiere conservar como una planta de interior, pueden desplegarse sus tallos y hojas hasta 2 m de longitud. También, lo ideal, es ubicarla en un lugar de media sombra, debido a que si no hay luz, sus hojas se puede perder su color y sus tonos crema característicos también. La clave de riesgo está en esperar a que la tierra esté seca para volver a regalarla. ', 'Product-miami.jpg', 10, 1),
-(6, 'Monstera deliciosa', '50', 'Es una especie de planta trepadora de la familia Araceae, endémica de selvas tropicales en el centro y sur de México. Es una de las tantas especies de plantas trepadoras de tallo grueso, en las mejores condiciones puede alcanzar los 20 metros de altura. Busca para ella un lugar cálido y luminoso, riégala una vez por semana y rocía las hojas con agua para evitar que se resequen y se rompan.', 'Monstera-deliciosa.jpg', 10, 1),
-(7, 'Suculenta', '25', 'Se han convertido en unas habituales tanto para los amantes de la jardinería como para principiantes pues ocupan poco espacio y pueden crecer en casi en cualquier sitio. Ellas son capaces de absorber la humedad del ambiente y retenerla en sus hojas, tallos y raíces; por eso para cuidarlas es necesario regarlas con poca agua, ubicarlas en un lugar donde reciban mucha luz y tenerlas en una matera donde puedan tener un buen drenaje.', 'suculenta.jpg', 10, 1),
-(8, 'Matera flores', '57', 'Matera con un aspecto retro pero reinventado, de forma redondeada asociada a una amplia gama de tamaños. Mezcla lo antiguo y lo nuevo para crear una matera de lujo, disponible en oro y plata. Las materas Gold tienen un aspecto elegante y las materas Silver dan un brillo más suave a tus espacios de interior.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Matera-flores.jpg', 10, 2),
-(9, 'Matera arcoíris', '75', 'Matera en cerámica con una textura desigual y suave al tacto; sus tonos varían a través de una gama de colores primarios, dándole vida y diversión a diferentes espacios de tu hogar. Es una opción versátil para adornar espacios interiores.\n\nDimensiones\nalto: 15 cm\nancho: 10 cm  ', 'Matera-arcoíris.jpg', 10, 2),
-(10, 'Matera espiral', '72', 'Con el set de materas Tierra, podrás darle vida a los rincones más escondidos de tu hogar. Sus colores varían de un verde atractivo a tonos suaves para generar espacios sutiles y llenos de conexión con la naturaleza. \n\nDimensiones\nalto: 8 cm\nancho: 5 cm ', 'Matera-espiral.jpg', 10, 2),
-(11, 'Matera gusanito', '80', 'Matera en cerámica con una textura desigual y suave al tacto; sus tonos varían a través de una gama de colores primarios, dándole vida y diversión a diferentes espacios de tu hogar. Es una opción versátil para adornar espacios interiores.\n\nDimensiones\nalto: 15 cm\nancho: 10 cm  ', 'Matera-gusanito.jpg', 10, 2),
-(12, 'Matera feliz', '78', 'Matera con un aspecto retro pero reinventado, de forma redondeada asociada a una amplia gama de tamaños. Mezcla lo antiguo y lo nuevo para crear una matera de lujo, disponible en oro y plata. Las materas Gold tienen un aspecto elegante y las materas Silver dan un brillo más suave a tus espacios de interior.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Matera-carita-feliz.jpg', 10, 2),
-(13, 'Matera Artevasi', '40', 'Matera en cerámica con una textura desigual y suave al tacto; sus tonos varían a través de una gama de colores primarios, dándole vida y diversión a diferentes espacios de tu hogar. Es una opción versátil para adornar espacios interiores.\n\nDimensiones\nalto: 15 cm\nancho: 10 cm  ', 'Matera-artevasi.jpg', 10, 2),
-(14, 'Materas Tierra', '25', 'Con el set de materas Tierra, podrás darle vida a los rincones más escondidos de tu hogar. Sus colores varían de un verde atractivo a tonos suaves para generar espacios sutiles y llenos de conexión con la naturaleza. \n\nDimensiones\nalto: 8 cm\nancho: 5 cm ', 'Materas-tierra.jpg', 10, 2),
-(15, 'Matera Gold retro', '40', 'Matera con un aspecto retro pero reinventado, de forma redondeada asociada a una amplia gama de tamaños. Mezcla lo antiguo y lo nuevo para crear una matera de lujo, disponible en oro y plata. Las materas Gold tienen un aspecto elegante y las materas Silver dan un brillo más suave a tus espacios de interior.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Matera-gold-retro.jpg', 10, 2),
-(16, 'Canasta para plantas', '85', 'Resalta la belleza de las plantas de tu hogar con esta canasta trenzada, elaborada por artesanos de la Sierra Nevada.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Accesorios-canasta.jpg', 10, 3),
-(17, 'Tejido colgante en macramé', '76', 'Resalta la belleza de las plantas de tu hogar con este tejido colgante en macramé elaborado por artesanos de la Sierra Nevada.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Accesorios-tejido-colgante-macrame.jpg', 10, 3),
-(18, 'Cuadro con plantas secas', '90', 'Resalta la belleza de las plantas de tu hogar con cuadros elaborados con flores secas. Y combínalos en los colores que quieras\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Accesorios-Cuadro-plantas-secas.jpg', 10, 3),
-(19, 'Repisa para plantas', '62', 'Lleva la belleza de las plantas a cada rincón de tu hogar, con esta repisa creada en hierro y madera\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Accesorios-repisa-plantas.jpg', 10, 3),
-(20, 'Fibra de coco', '20', 'Este sustrato ayuda al buen desarrollo de las raíces de la planta. Su acción consiste en mejorar la estructura del suelo , facilitando así tanto el crecimiento como el asentamiento de las raíces. Cómpralo por 1 kilo', 'Sustrato-fibra-coco.jpg', 10, 4),
-(21, 'Tierra abonada', '30', 'Sustrato orgánico natural cuidadosamente seleccionado, tamizado y procesado con componentes estructurales y nutritivos que garantizan propiedades físicas y químicas necesarias para el adecuado desarrollo de las plantas. Utiliza el sustrato para macetas en el cultivo de plantas que requieran buena retención de humedad.', 'Sustrato-tierra-abonada.jpg', 10, 4),
-(22, 'Cascarilla de arroz', '25', 'Este sustrato no solo controla la presencia de malezas sino que mejora la retención de agua de la planta, disminuye la temperatura del suelo, permite un adecuado crecimiento, ayuda a la fijación de nitrógeno, fósforo y potasio. Cómpralo por 1 kilo', 'Sustrato-cascarilla-arroz.jpg', 10, 4);
-
--- --------------------------------------------------------
-
 
 -- Volcado de datos para la tabla `productscategories`
 --
@@ -62,18 +33,47 @@ INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`, `phone`, `ava
 (5, 'Maira', 'Admin', 'mai@admin.com', '10', 10, 0x6d616941646d696e2e706e67, 1),
 (6, 'Maira', 'Generico', 'mai@gen.com', '10', 10, 0x6d616947656e2e706e67, 2);
 
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `stock`, `products_categories_id`) VALUES
+(1, 'Ave del Paraíso', '50', 'Esta planta originaria de sudáfrica, se cultiva extensamente como planta ornamental por la forma peculiar de su flor.  Es una belleza natural de la que es imposible no enamorarse. Si bien en una maceta puede alcanzar una altura máxima de metro y medio, plantada directamente en suelo puede superar los dos metros y medio. Como buena planta tropical, necesita al menos cuatro horas de sol diarias especialmente en los meses de frío. ', 'product-ave-paraiso.png', 10, 1),
+(2, 'Ficus elastica', '45', 'Es una de esas plantas de interior irresistible. Por un lado, su belleza ornamental es capaz de decorar con la elegancia de sus hojas intensamente verdes. Por otro, los cuidados del ficus elástica son tan sencillos que hacen de esta planta una poco trabajosa. Busca para ella un lugar de muchísima luz, mejor si es al sol directo. Además el riego debe ser moderado', 'ficus-elastica.png', 10, 1),
+(3, 'Syngonium', '55', 'Esta no solo es sumamente hermosa y llamativa. Además es fácil de cuidar así que será sencillo para ti disfrutar de una belleza verde que decora con su mera presencia gracias, en gran medida, a sus hojas. Para cultivarla busca un espacio con buena luminosidad, no permitas que le de el sol directo y dale una buena dosis de humedad en la tierra, pero no demasiada.', 'product-syngonium.png', 10, 1),
+(4, 'Begonia Maculata', '70', 'Esta planta es un auténtico espectáculo natural. Sus singulares lunares plateados resaltan sobre el intenso verde de sus hojas y  sus hojas caen en cascada a partir de su tronco central. Además, sus flores en forma de campana nacen en forma de racimos ininterrumpidamente desde primavera hasta final del verano. Debes cultivarla dentro de tu casa en un lugar de mucha luminosidad pero nunca con sol directo.', 'product-begonia-maculata.png', 10, 1),
+(5, 'Miami', '30', 'Esta es una planta muy versátil, por lo que se puede tener en el interior o exterior. Cuando el Miami se quiere conservar como una planta de interior, pueden desplegarse sus tallos y hojas hasta 2 m de longitud. También, lo ideal, es ubicarla en un lugar de media sombra, debido a que si no hay luz, sus hojas se puede perder su color y sus tonos crema característicos también. La clave de riesgo está en esperar a que la tierra esté seca para volver a regalarla. ', 'Product-miami.jpg', 10, 1),
+(6, 'Monstera deliciosa', '50', 'Es una especie de planta trepadora de la familia Araceae, endémica de selvas tropicales en el centro y sur de México. Es una de las tantas especies de plantas trepadoras de tallo grueso, en las mejores condiciones puede alcanzar los 20 metros de altura. Busca para ella un lugar cálido y luminoso, riégala una vez por semana y rocía las hojas con agua para evitar que se resequen y se rompan.', 'Monstera-deliciosa.jpg', 10, 1),
+(7, 'Suculenta', '25', 'Se han convertido en unas habituales tanto para los amantes de la jardinería como para principiantes pues ocupan poco espacio y pueden crecer en casi en cualquier sitio. Ellas son capaces de absorber la humedad del ambiente y retenerla en sus hojas, tallos y raíces; por eso para cuidarlas es necesario regarlas con poca agua, ubicarlas en un lugar donde reciban mucha luz y tenerlas en una matera donde puedan tener un buen drenaje.', 'suculenta.jpg', 10, 1),
+(8, 'Matera flores', '57', 'Matera con un aspecto retro pero reinventado, de forma redondeada asociada a una amplia gama de tamaños. Mezcla lo antiguo y lo nuevo para crear una matera de lujo, disponible en oro y plata. Las materas Gold tienen un aspecto elegante y las materas Silver dan un brillo más suave a tus espacios de interior.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Matera-flores.jpg', 10, 2),
+(9, 'Matera arcoíris', '75', 'Matera en cerámica con una textura desigual y suave al tacto; sus tonos varían a través de una gama de colores primarios, dándole vida y diversión a diferentes espacios de tu hogar. Es una opción versátil para adornar espacios interiores.\n\nDimensiones\nalto: 15 cm\nancho: 10 cm  ', 'Matera-arcoíris.jpg', 10, 2),
+(10, 'Matera espiral', '72', 'Con el set de materas Tierra, podrás darle vida a los rincones más escondidos de tu hogar. Sus colores varían de un verde atractivo a tonos suaves para generar espacios sutiles y llenos de conexión con la naturaleza. \n\nDimensiones\nalto: 8 cm\nancho: 5 cm ', 'Matera-espiral.jpg', 10, 2),
+(11, 'Matera gusanito', '80', 'Matera en cerámica con una textura desigual y suave al tacto; sus tonos varían a través de una gama de colores primarios, dándole vida y diversión a diferentes espacios de tu hogar. Es una opción versátil para adornar espacios interiores.\n\nDimensiones\nalto: 15 cm\nancho: 10 cm  ', 'Matera-gusanito.jpg', 10, 2),
+(12, 'Matera feliz', '78', 'Matera con un aspecto retro pero reinventado, de forma redondeada asociada a una amplia gama de tamaños. Mezcla lo antiguo y lo nuevo para crear una matera de lujo, disponible en oro y plata. Las materas Gold tienen un aspecto elegante y las materas Silver dan un brillo más suave a tus espacios de interior.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Matera-carita-feliz.jpg', 10, 2),
+(13, 'Matera Artevasi', '40', 'Matera en cerámica con una textura desigual y suave al tacto; sus tonos varían a través de una gama de colores primarios, dándole vida y diversión a diferentes espacios de tu hogar. Es una opción versátil para adornar espacios interiores.\n\nDimensiones\nalto: 15 cm\nancho: 10 cm  ', 'Matera-artevasi.jpg', 10, 2),
+(14, 'Materas Tierra', '25', 'Con el set de materas Tierra, podrás darle vida a los rincones más escondidos de tu hogar. Sus colores varían de un verde atractivo a tonos suaves para generar espacios sutiles y llenos de conexión con la naturaleza. \n\nDimensiones\nalto: 8 cm\nancho: 5 cm ', 'Materas-tierra.jpg', 10, 2),
+(15, 'Matera Gold retro', '40', 'Matera con un aspecto retro pero reinventado, de forma redondeada asociada a una amplia gama de tamaños. Mezcla lo antiguo y lo nuevo para crear una matera de lujo, disponible en oro y plata. Las materas Gold tienen un aspecto elegante y las materas Silver dan un brillo más suave a tus espacios de interior.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Matera-gold-retro.jpg', 10, 2),
+(16, 'Canasta para plantas', '85', 'Resalta la belleza de las plantas de tu hogar con esta canasta trenzada, elaborada por artesanos de la Sierra Nevada.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Accesorios-canasta.jpg', 10, 3),
+(17, 'Tejido colgante en macramé', '76', 'Resalta la belleza de las plantas de tu hogar con este tejido colgante en macramé elaborado por artesanos de la Sierra Nevada.\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Accesorios-tejido-colgante-macrame.jpg', 10, 3),
+(18, 'Cuadro con plantas secas', '90', 'Resalta la belleza de las plantas de tu hogar con cuadros elaborados con flores secas. Y combínalos en los colores que quieras\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Accesorios-Cuadro-plantas-secas.jpg', 10, 3),
+(19, 'Repisa para plantas', '62', 'Lleva la belleza de las plantas a cada rincón de tu hogar, con esta repisa creada en hierro y madera\n\nDimensiones\nalto: 20 cm\nancho: 15 cm  ', 'Accesorios-repisa-plantas.jpg', 10, 3),
+(20, 'Fibra de coco', '20', 'Este sustrato ayuda al buen desarrollo de las raíces de la planta. Su acción consiste en mejorar la estructura del suelo , facilitando así tanto el crecimiento como el asentamiento de las raíces. Cómpralo por 1 kilo', 'Sustrato-fibra-coco.jpg', 10, 4),
+(21, 'Tierra abonada', '30', 'Sustrato orgánico natural cuidadosamente seleccionado, tamizado y procesado con componentes estructurales y nutritivos que garantizan propiedades físicas y químicas necesarias para el adecuado desarrollo de las plantas. Utiliza el sustrato para macetas en el cultivo de plantas que requieran buena retención de humedad.', 'Sustrato-tierra-abonada.jpg', 10, 4),
+(22, 'Cascarilla de arroz', '25', 'Este sustrato no solo controla la presencia de malezas sino que mejora la retención de agua de la planta, disminuye la temperatura del suelo, permite un adecuado crecimiento, ayuda a la fijación de nitrógeno, fósforo y potasio. Cómpralo por 1 kilo', 'Sustrato-cascarilla-arroz.jpg', 10, 4);
+
+-- --------------------------------------------------------
+
 
 -- Indices de la tabla `usercategories`
 --
-ALTER TABLE `usercategories`
-  ADD PRIMARY KEY (`id`);
+--ALTER TABLE `usercategories`
+ -- ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `users`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_223fee66-696b-42b2-9602-583b5ff05ff4` (`user_categories_id`);
+--ALTER TABLE `users`
+ -- ADD PRIMARY KEY (`id`),
+ -- ADD KEY `FK_223fee66-696b-42b2-9602-583b5ff05ff4` (`user_categories_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -82,25 +82,25 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+--ALTER TABLE `products`
+  --MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `productscategories`
 --
-ALTER TABLE `productscategories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--ALTER TABLE `productscategories`
+ -- MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `usercategories`
 --
-ALTER TABLE `usercategories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--ALTER TABLE `usercategories`
+ -- MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
-ALTER TABLE `users`
+--ALTER TABLE `users`--
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
@@ -110,15 +110,14 @@ ALTER TABLE `users`
 --
 -- Filtros para la tabla `products`
 --
-ALTER TABLE `products`
-  ADD CONSTRAINT `FK_882a4ba8-b7f1-4332-8ab4-3575b7f80058` FOREIGN KEY (`products_categories_id`) REFERENCES `productscategories` (`id`);
+--ALTER TABLE `products`--
+  --ADD CONSTRAINT `FK_882a4ba8-b7f1-4332-8ab4-3575b7f80058` FOREIGN KEY (`products_categories_id`) REFERENCES `productscategories` (`id`);
 
 --
 -- Filtros para la tabla `users`
 --
-ALTER TABLE `users`
-  ADD CONSTRAINT `FK_223fee66-696b-42b2-9602-583b5ff05ff4` FOREIGN KEY (`user_categories_id`) REFERENCES `usercategories` (`id`);
+--ALTER TABLE `users`--
+ -- ADD CONSTRAINT `FK_223fee66-696b-42b2-9602-583b5ff05ff4` FOREIGN KEY (`user_categories_id`) REFERENCES `usercategories` (`id`);
 --
-
 
 
